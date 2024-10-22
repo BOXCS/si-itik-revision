c69986b58d8b492146d66097c9aae0c33d9f4be8
zulfadeo <159153579+zdeo4@users.noreply.github.com>
2024-10-22 22:20:53 +0700

    Dashboard sementara

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useSearchParams } from "next/navigation";
 import { SidebarDemo } from "@/components/Sidebar";
 import SidebarHandler from "@/components/SidebarHandler";
-<<<<<<< Updated upstream
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "@/lib/firebase"; // Pastikan path ini sesuai dengan konfigurasi Firebase Anda
-=======
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
->>>>>>> Stashed changes
 import RootLayout from "../layout";
 import { Card } from '@/components/ui/card';
 import Image from "next/image";
@@ -17,25 +12,6 @@ import { AreaChart } from "@/components/ui/chart"
 
 export default function Dashboard() {
   const searchParams = useSearchParams();
-<<<<<<< Updated upstream
-  const [username, setUsername] = useState<string>("User");
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // Jika user memiliki displayName, gunakan itu, jika tidak gunakan email sebagai pengganti
-        setUsername(user.displayName || user.email || "User");
-      } else {
-        // Jika tidak ada user yang login, mungkin Anda ingin mengarahkan ke halaman login atau tetap menampilkan "User"
-        setUsername("User");
-      }
-    });
-
-    // Bersihkan subscription saat komponen unmount
-    return () => unsubscribe();
-  }, []);
-
-=======
   const username = searchParams?.get("username") || "User";
   const images = [
     "/assets/DB_layer.webp",
@@ -72,7 +48,6 @@ export default function Dashboard() {
     { date: "Dec", Revenue: 3239, Cost: 3736, },
   ]
 
->>>>>>> Stashed changes
   return (
     <div>
       <SidebarDemo>
