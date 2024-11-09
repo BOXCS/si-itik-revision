e5638765ef7ddc6e07b41ffe0d78e2592e7293dd
zulfadeo <159153579+zdeo4@users.noreply.github.com>
2024-11-09 14:43:16 +0700

    Dashboard
    Dashboard with chart & analysis history
    (not fix)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs, orderBy, query, where, Timestamp } from "firebase/firestore";
+import { firestore } from "@/lib/firebase";
 import { useSearchParams } from "next/navigation";
 import { SidebarDemo } from "@/components/Sidebar";
-import SidebarHandler from "@/components/SidebarHandler";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import RootLayout from "../layout";
-import { Card } from '@/components/ui/card';
-import Image from "next/image";
 import { AreaChart } from "@/components/ui/chart"
-<<<<<<< Updated upstream
-=======
 import { Tooltip } from "@/components/ui/tooltip";
 import UserAvatar from "@/components/ui/avatar";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
@@ -27,7 +23,6 @@ import {
 } from "@mui/material";
 import { auth } from "@/lib/firebase";
 import { unsubscribe } from "diagnostics_channel";
->>>>>>> Stashed changes
 
 interface AnalisisPeriode {
   id: string;
@@ -54,15 +49,6 @@ interface AnalysisPeriodData {
 
 export default function Dashboard() {
   const searchParams = useSearchParams();
-<<<<<<< Updated upstream
-  const username = searchParams?.get("username") || "User";
-  const images = [
-    "/assets/DB_layer.webp",
-    "/assets/DB_penggemukan.webp",
-    "/assets/Layer.svg",
-  ];
-
-=======
   const [userEmail, setUserEmail] = useState<string | null>(null);
   const [userPhoto, setUserPhoto] = useState<string | null>(null);
   const [data, setData] = useState<AnalysisPeriodData[]>([]);
@@ -462,7 +448,6 @@ useEffect(() => {
   }, []);
 
 
->>>>>>> Stashed changes
   function formatNumber(number: number): string {
     if (number >= 1000000) { 
       const millions = number / 1000000;
@@ -475,37 +460,11 @@ useEffect(() => {
     }
   }
 
-<<<<<<< Updated upstream
-  // return <div>Selamat Datang {username}</div>;
-  // <a href="">analisis penetasan</a>
-  const chartdata = [
-    { date: "Jan", Revenue: 2890, Cost: 2338, },
-    { date: "Feb", Revenue: 2756, Cost: 2103, },
-    { date: "Mar", Revenue: 3322, Cost: 2194, },
-    { date: "Apr", Revenue: 3470, Cost: 2108, },
-    { date: "May", Revenue: 3475, Cost: 1812, },
-    { date: "Jun", Revenue: 3129, Cost: 1726, },
-    { date: "Jul", Revenue: 3490, Cost: 1982, },
-    { date: "Aug", Revenue: 2903, Cost: 2012, },
-    { date: "Sep", Revenue: 2643, Cost: 2342, },
-    { date: "Oct", Revenue: 2837, Cost: 2473, },
-    { date: "Nov", Revenue: 2954, Cost: 3848, },
-    { date: "Dec", Revenue: 3239, Cost: 3736, },
-  ]
-
-=======
->>>>>>> Stashed changes
   return (
     <div>
       <SidebarDemo>
         <div className="flex-1 items-center justify-center">
           {/* Title Menu */}
-<<<<<<< Updated upstream
-          <div className="flex flex-wrap justify-between p-5">
-            <h1 className="text-1xl font-bold">Beranda
-            </h1>
-            <h1 className="text-sm ">{username} </h1>
-=======
           <div className="flex flex-wrap justify-between p-5 pt-5 pb-0">
             <h1 className="text-1xl font-bold">Beranda </h1>
             <Tooltip
@@ -520,13 +479,11 @@ useEffect(() => {
                 </div>
               </div>
             </Tooltip>
->>>>>>> Stashed changes
           </div>
 
           <div className="flex flex-cols grid-rows-2 pb-0">
-
             {/* Main */}
-            <div className="flex-1 flex-wrap grid-cols items-center justify-center p-5 pb-0">
+            <div className="flex-1 flex-wrap 4grid-cols items-center justify-center p-5 pb-0">
               {/* Card Tab */}
               <div className="flex-1 bg-white to-orange-100 rounded-lg shadow-md height-1/2 p-8">
                 <div className="flex-1">
@@ -542,8 +499,8 @@ useEffect(() => {
                         className="space-y-2 text-sm leading-7 text-gray-600 dark:text-gray-500"
                       >
                         <div className="flex space-x-4">
-                          <img src="/assets/DB_layer.webp"
-                            alt="DB_layeri"
+                          <img src="/assets/DB_penggemukan.webp"
+                            alt="DB_penggemukan"
                             style={{ width: '100px', height: '100%', }} />
                           <p>
                             Penetasan merupakan fitur yang dirancang untuk mengoptimalkan proses penetasan telur itik, memastikan kesuksesan menetas maksimal dan kualitas anakan itik yang terbaik.
@@ -565,7 +522,6 @@ useEffect(() => {
                       </TabsContent>
                       <TabsContent
                         value="tab3"
-
                         className="space-y-2 text-sm leading-7 text-gray-600 dark:text-gray-500"
                       >
                         <div className="flex space-x-4">
@@ -582,20 +538,6 @@ useEffect(() => {
                 </div>
               </div>
 
-<<<<<<< Updated upstream
-
-              <div className="grid grid-cols-2 justify-center gap-5">
-                <div className="bg-white p-3 rounded-lg shadow-md">
-                  <AreaChart
-                    className=" flex items-center justify-center h-50"
-                    data={chartdata}
-                    index="date"
-                    categories={["Revenue", "Cost"]}
-                    valueFormatter={(number: number) =>
-                      `Rp${formatNumber(number)}`}
-                    onValueChange={(v) => console.log(v)}
-                  />
-=======
               <div className="grid grid-cols-2 justify-center gap-5 pt-5">
                 <div className="bg-white p-3 rounded-lg shadow-md">
                   <div className="flex-1">
@@ -667,29 +609,14 @@ useEffect(() => {
                       </div>
                     </Tabs>
                   </div>
->>>>>>> Stashed changes
                 </div>
                 <div className="bg-white p-3 rounded-lg shadow-md">
                 </div>
               </div>
             </div>
 
+            {/* Riwayat */}
             <div className="flex justify-center p-5 gap-5 pl-0">
-<<<<<<< Updated upstream
-              <div className="bg-white p-3 rounded-lg shadow-md">
-                <AreaChart
-                  className=" flex items-center justify-center h-50 w-80"
-                  data={chartdata}
-                  index="date"
-                  categories={["Revenue", "Cost"]}
-                  valueFormatter={(number: number) =>
-                    `Rp${formatNumber(number)}`}
-                  onValueChange={(v) => console.log(v)}
-                />
-              </div>
-            </div>
-          </div>
-=======
               <div className="bg-white p-3 rounded-lg shadow-md w-200">
                 <h2 className="text-lg sm-bold">Riwayat Analisis</h2>
                 <Grid container spacing={3}>
@@ -774,7 +701,6 @@ useEffect(() => {
           <div>
             <h1 className="text-sm flex-center">@si-itik.polije</h1>
           </div>
->>>>>>> Stashed changes
         </div>
       </SidebarDemo>
     </div>
